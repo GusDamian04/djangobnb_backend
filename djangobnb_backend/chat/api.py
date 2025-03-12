c8c3638efbb645837721548024c4225fa0f22340
Gus Damian <damiangustavoangel@gmail.com>
2025-03-11 22:32:00 -0600

    12 Real time chat

--- djangobnb_backend/chat/api.py
+++ djangobnb_backend/chat/api.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 
 from .models import Conversation, ConversationMessage
-from .serializers import ConversationListSerializer
+from .serializers import ConversationListSerializer, ConversationDetailSerializer, ConversationMessageSerializer
 
 from useraccount.models import User
 
@@ -14,3 +14,24 @@ def conversations_list(request):
 
     return JsonResponse(serializer.data, safe=False)
 
+
+@api_view(['GET'])
+def conversations_detail(request, pk):
+    conversation = request.user.conversations.get(pk=pk)
+
+    conversation_serializer = ConversationDetailSerializer(conversation, many=False)
+    messages_serializer = ConversationMessageSerializer(conversation.messages.all(), many=True)
+
+    return JsonResponse({
+        'conversation': conversation_serializer.data,
+        'messages': messages_serializer.data
+    }, safe=False)
+
+@api_view(['GET'])
+def conversations_start(request, user_id):
+    user = User.objects.get(pk=user_id)
+    conversation = Conversation.objects.create()
+    conversation.users.add(request.user)
+    conversation.users.add(user)
+
+    return JsonResponse({'success': True, 'conversation_id': conversation.id})
